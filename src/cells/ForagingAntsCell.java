158a10cc47b42cbf17b410220dc6ca8265f19446
Brenna Milligan <brenna.milligan@comcast.net>
2015-09-23 21:33:02 -0400

    Foraging Ants Simulation

---
+++ src/cells/ForagingAntsCell.java
@@ -0,0 +1,124 @@
+package cells;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ForagingAntsCell extends Cell {
+    public static final int NUMBER_FORWARD_LOCATIONS = 3;
+    
+    private boolean myFoodItem;
+    private int myNestPheromones;
+    private int myFoodPheromones;
+    private int myLives;
+    private int myOrientation;
+    private List<Integer> myForwardLocations;
+    private List<Integer> myNeighborLocations;
+    private boolean myObstacle;
+    private int myNumberOfAnts;
+    
+    public ForagingAntsCell() {
+        myFoodItem = false;
+        myNestPheromones = 0;
+        myFoodPheromones = 0;
+        myOrientation = 1;
+        myForwardLocations = new ArrayList<Integer>();
+        updateForwardLocations();
+        myNumberOfAnts = 0;
+        setMyNeighborLocations();
+        myLives = 0;
+    }
+    
+    public int getMyLives () {
+        return myLives;
+    }
+
+    public void incrementLives () {
+        myLives++;
+    }
+    
+    public void resetLives() {
+        myLives = 0;
+    }
+
+    public void incrementMyNumberOfAnts () {
+        myNumberOfAnts++;
+    }
+    
+    public void decrementMyNumberOfAnts () {
+        myNumberOfAnts--;
+    }
+
+    private void setMyNeighborLocations() {
+        for (int i = 0; i < getMyNeighbors().length; i++) {
+            myNeighborLocations.add(i);
+        }
+    }
+    
+    public boolean isAnObstacle() {
+        return myObstacle;
+    }
+    
+    public boolean hasFoodItem() {
+        return myFoodItem;
+    }
+
+    public int getMyNestPheromones () {
+        return myNestPheromones;
+    }
+
+    public int getMyFoodPheromones () {
+        return myFoodPheromones;
+    }
+    
+    public int getMyOrientation () {
+        return myOrientation;
+    }
+    
+    public List<Integer> getMyForwardLocations () {
+        return myForwardLocations;
+    }
+    
+    public List<Integer> getMyNeighborLocations () {
+        return myNeighborLocations;
+    }
+    
+    public int getMyNumberOfAnts () {
+        return myNumberOfAnts;
+    }
+    
+    public void setMyOrientation (int myOrientation) {
+        this.myOrientation = myOrientation;
+        updateForwardLocations();
+    }
+    
+    private void updateForwardLocations() {
+        myForwardLocations.add(myOrientation - 1);
+        myForwardLocations.add(myOrientation);
+        myForwardLocations.add(myOrientation + 1);
+        if (myOrientation == 0) {
+            myForwardLocations.set(0, getMyNeighbors().length - 1);
+        }
+        else if (myOrientation == getMyNeighbors().length - 1) {
+            myForwardLocations.set(2, 0);
+        }
+    }
+
+    public void setMyNestPheromones (int myNestPheromones) {
+        this.myNestPheromones = myNestPheromones;
+    }
+
+    public void setMyFoodPheromones (int myFoodPheromones) {
+        this.myFoodPheromones = myFoodPheromones;
+    }
+    
+    public void setMyFoodItem (boolean myFoodItem) {
+        this.myFoodItem = myFoodItem;
+    }
+
+    @Override
+    public void initializeWithState (int state) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
--- src/simulations/ForagingAnts.java
+++ src/simulations/ForagingAnts.java
@@ -1,13 +1,18 @@
 package simulations;
 
 import java.util.HashMap;
+import java.util.List;
 import cells.Cell;
+import cells.ForagingAntsCell;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 
+
 public class ForagingAnts extends Simulation {
-    public static final int TOTAL_STATES = 3;
-    private static final Paint[] COLORS = { Color.WHITE, Color.LIGHTGREEN, Color.DIMGREY };
+    public static final int TOTAL_STATES = 2;
+    public static final int BLANK = 0;
+    public static final int ANT = 1;
+    private static final Paint[] COLORS = { Color.WHITE, Color.DARKRED };
     public static final String ANT_LIFE = "ANT_LIFE";
     public static final String MAX_ANTS = "MAX_ANTS";
     public static final String ANTS_BORN_PER_TIME = "ANTS_BORN_PER_TIME";
@@ -17,9 +22,9 @@ public class ForagingAnts extends Simulation {
     public static final String DIFFUSION_RATIO = "DIFFUSION_RATIO";
     public static final String K = "K";
     public static final String N = "N";
-    
-    
-    //For Jasper to add to XML
+    public static final int CONSTANT = 2;
+
+    // For Jasper to add to XML
     private int myAntLife = 500;
     private int myMaxAnts = 10;
     private int myAntsBornPerTime = 2;
@@ -29,17 +34,30 @@ public class ForagingAnts extends Simulation {
     private double myDiffusionRatio = .1;
     private double myK = .001;
     private double myN = 10;
-    
-    
 
     public ForagingAnts () {
-        super(TOTAL_STATES, COLORS);        
+        super(TOTAL_STATES, COLORS);
     }
 
     @Override
     public void checkRules (Cell cell) {
-        // TODO Auto-generated method stub
+        if (cell.checkMyCurrentState(ANT)) {
+            if (((ForagingAntsCell) cell).getMyLives() == myAntLife) {
+                kill((ForagingAntsCell) cell);
+            }
+            else {
+                antForage((ForagingAntsCell) cell);
+            }
+        }
+        ((ForagingAntsCell) cell).incrementLives();
 
+    }
+
+    private void kill (ForagingAntsCell cell) {
+        cell.setMyNextState(BLANK);
+        cell.resetLives();
+        cell.setMyFoodItem(false);
+        cell.decrementMyNumberOfAnts();
     }
 
     @Override
@@ -59,6 +77,164 @@ public class ForagingAnts extends Simulation {
         myDiffusionRatio = parameterMap.get(DIFFUSION_RATIO);
         myK = parameterMap.get(K);
         myN = parameterMap.get(N);
+    }
+
+    private void antForage (ForagingAntsCell ant) {
+        if (ant.hasFoodItem()) {
+            returnToNest(ant);
+        }
+        else {
+            findFoodSource(ant);
+        }
+    }
+
+    private boolean atFoodSource (ForagingAntsCell ant) {
+        return ant.getMyFoodPheromones() == myMaxPheromone;
+    }
+
+    private boolean atNest (ForagingAntsCell ant) {
+        return ant.getMyNestPheromones() == myMaxPheromone;
+    }
+
+    private void returnToNest (ForagingAntsCell ant) {
+        if (atFoodSource(ant)) {
+            ant.setMyOrientation(setOrientationNest(ant));
+        }
+        int locationToMove = selectLocationNest((ForagingAntsCell[]) ant.getMyNeighbors(), ant.getMyForwardLocations());
+        if (locationToMove == -1) {
+            locationToMove = selectLocationNest((ForagingAntsCell[]) ant.getMyNeighbors(), ant.getMyNeighborLocations());
+        }
+        if (locationToMove != -1) {
+            dropFoodPheromones(ant);
+            ant.setMyOrientation(locationToMove);
+            ForagingAntsCell neighborToMoveTo = (ForagingAntsCell) ant.getMyNeighbors()[locationToMove];
+            neighborToMoveTo.setMyNextState(ANT);
+            ant.setMyNextState(BLANK);
+            neighborToMoveTo.incrementMyNumberOfAnts();
+            ant.decrementMyNumberOfAnts();
+            if (atNest(neighborToMoveTo)) {
+                neighborToMoveTo.setMyFoodItem(false);
+            }
+        }
+    }
+
+    private void findFoodSource (ForagingAntsCell ant) {
+        if (atNest(ant)) {
+            ant.setMyOrientation(setOrientationFood(ant));
+        }
+        int locationToMove = selectLocationFood((ForagingAntsCell[]) ant.getMyNeighbors(), ant.getMyForwardLocations());
+        if (locationToMove == -1) {
+            locationToMove = selectLocationFood((ForagingAntsCell[]) ant.getMyNeighbors(), ant.getMyNeighborLocations());
+        }
+        if (locationToMove != -1) {
+            dropHomePheromones(ant);
+            ant.setMyOrientation(locationToMove);
+            ForagingAntsCell neighborToMoveTo = (ForagingAntsCell) ant.getMyNeighbors()[locationToMove];
+            neighborToMoveTo.setMyNextState(ANT);
+            ant.setMyNextState(BLANK);
+            neighborToMoveTo.incrementMyNumberOfAnts();
+            ant.decrementMyNumberOfAnts();
+            if (atFoodSource(neighborToMoveTo)) {
+                neighborToMoveTo.setMyFoodItem(true);
+            }
+        }
+    }
+
+    private int setOrientationFood (ForagingAntsCell ant) {
+        int orientation = -1;
+        ForagingAntsCell[] neighbors = (ForagingAntsCell[]) ant.getMyNeighbors();
+        for (int i = 0; i <neighbors.length; i++) {
+            if (neighbors[i] != null) {
+                if (neighbors[i].getMyFoodPheromones() > neighbors[orientation].getMyFoodPheromones()) {
+                    orientation = i;
+                }
+            }
+        }
+        return orientation;
+    }
+
+    private int setOrientationNest (ForagingAntsCell ant) {
+        int orientation = -1;
+        ForagingAntsCell[] neighbors = (ForagingAntsCell[]) ant.getMyNeighbors();
+        for (int i = 0; i <neighbors.length; i++) {
+            if (neighbors[i] != null) {
+                if (neighbors[i].getMyNestPheromones() > neighbors[orientation].getMyNestPheromones()) {
+                    orientation = i;
+                }
+            }
+        }
+        return orientation;
+    }
+
+    private int selectLocationNest(ForagingAntsCell[] neighbors, List<Integer> locationSet) {
+        int location = -1;
+        for (int i : locationSet) {
+            if (neighbors[i] != null) locationSet.remove(i);
+            if (neighbors[i].isAnObstacle() || neighbors[i].getMyNumberOfAnts() >= myMaxAnts) {
+                locationSet.remove(i);
+            }
+        }
+        if (!locationSet.isEmpty()) {
+            int max = 0;
+            for (int i : locationSet) {
+                if (neighbors[i].getMyNestPheromones() > max) {
+                    location = i;
+                }
+            }
+        }
+        return location;
+    }
+
+    private int selectLocationFood(ForagingAntsCell[] neighbors, List<Integer> locationSet) {
+        int location = -1;
+        for (int i : locationSet) {
+            if (neighbors[i] != null) locationSet.remove(i);
+            if (neighbors[i].isAnObstacle() || neighbors[i].getMyNumberOfAnts() >= myMaxAnts) {
+                locationSet.remove(i);
+            }
+        }
+        if (!locationSet.isEmpty()) {
+            int maxProb = 0;
+            for (int i : locationSet)
+                if (Math.pow(myK + neighbors[i].getMyFoodPheromones(), myN) > maxProb) location = i;
+        }
+        return location;
+    }
+
+    private void dropHomePheromones(ForagingAntsCell ant) {
+        if (atNest(ant)) {
+            ant.setMyNestPheromones(myMaxPheromone);
+        }
+        else {
+            int max = 0;
+            for (ForagingAntsCell neighbor : (ForagingAntsCell[]) ant.getMyNeighbors()) {
+                int neighborsNestPheromones = neighbor.getMyNestPheromones();
+                if (neighborsNestPheromones > max) max = neighborsNestPheromones;
+            }
+            int desired = max - CONSTANT;
+            if (desired > ant.getMyNestPheromones()) {
+                ant.setMyNestPheromones(desired);
+            }
+        }
+    }
+
+    private void dropFoodPheromones(ForagingAntsCell ant) {
+        if (atFoodSource(ant)) {
+            ant.setMyFoodPheromones(myMaxPheromone);
+        }
+        else {
+            int max = 0;
+            for (ForagingAntsCell neighbor : (ForagingAntsCell[]) ant.getMyNeighbors()) {
+                if (neighbor != null) {
+                    int neighborsFoodPheromones = neighbor.getMyFoodPheromones();
+                    if (neighborsFoodPheromones > max) max = neighborsFoodPheromones;
+                }
+            }
+            int desired = max - CONSTANT;
+            if (desired > ant.getMyFoodPheromones()) {
+                ant.setMyFoodPheromones(desired);
+            }
+        }
     }
 
 }
