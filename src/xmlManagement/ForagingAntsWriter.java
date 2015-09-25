245c59b62623335473394efe74c44b7dedf3a0ff
Jasper Hancock <jasper.hancock@duke.edu>
2015-09-25 17:05:27 -0400

    Created ForagingAnts.xml

--- src/xmlManagement/ForagingAntsWriter.java
+++ src/xmlManagement/ForagingAntsWriter.java
@@ -4,15 +4,15 @@ import simulations.ForagingAnts;
 
 public class ForagingAntsWriter extends SimWriter {
     
-    private double myAntLife = 500;
-    private double myMaxAnts = 10;
-    private double myAntsBornPerTime = 2;
-    private double myMinPheromones = 0;
-    private double myMaxPheromones = 1000;
-    private double myEvaporationRatio = .1;
-    private double myDiffusionRatio = .1;
-    private double myK = .001;
-    private double myN = 10;
+    private final double myAntLife = 500;
+    private final double myMaxAnts = 10;
+    private final double myAntsBornPerTime = 2;
+    private final double myMinPheromones = 0;
+    private final double myMaxPheromones = 1000;
+    private final double myEvaporationRatio = .1;
+    private final double myDiffusionRatio = .1;
+    private final double myK = .001;
+    private final double myN = 10;
 
     public ForagingAntsWriter () {
         super("XML/ForagingAnts.xml", "Watch the little people run", "Walk on the wild side", "Brenna Milligan", 25, 25, 2);
@@ -23,6 +23,7 @@ public class ForagingAntsWriter extends SimWriter {
     public void populateParameterMap () {
         
         super.getParameterMap().put(ForagingAnts.ANT_LIFE, myAntLife);
+        System.out.println(super.getParameterMap().get(ForagingAnts.ANT_LIFE));
         super.getParameterMap().put(ForagingAnts.ANTS_BORN_PER_TIME,myAntsBornPerTime);
         super.getParameterMap().put(ForagingAnts.MAX_ANTS, myMaxAnts);
         super.getParameterMap().put(ForagingAnts.MIN_PHEROMONE, myMinPheromones);
