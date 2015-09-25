ef10895e1bb2c44605dd6342a66d86e660348c58
Brenna Milligan <brenna.milligan@comcast.net>
2015-09-25 16:15:25 -0400

    updates to slime mold

--- src/cells/SlimeMoldCell.java
+++ src/cells/SlimeMoldCell.java
@@ -1,6 +1,12 @@
 package cells;
 
 public class SlimeMoldCell extends Cell {
+    
+    private int myCampAmount;
+    
+    public SlimeMoldCell() {
+        myCampAmount = 0;
+    }
 
     @Override
     public void initializeWithState (int state) {
--- src/simulations/SlimeMold.java
+++ src/simulations/SlimeMold.java
@@ -12,6 +12,15 @@ public class SlimeMold extends Simulation {
     private static final Paint[] COLORS =
             { Color.WHITE, Color.BLUE, Color.BLACK, Color.ORANGE, Color.PURPLE };
     private static final int EMPTY = 0;
+    public static final String WIGGLE_BIAS = "WIGGLE_BIAS";
+    public static final String WIGGLE_ANGLE = "WIGGLE_ANGLE";
+    public static final String SNIFF_THRESHOLD = "SNIFF_THRESHOLD";
+    public static final String SNIFF_ANGLE = "SNIFF_ANGLE";
+    
+    private double myWiggleBias;
+    private double myWiggleAngle;
+    private double mySniffThreshold;
+    private double mySniffAngle;
     
     public SlimeMold () {
         super(TOTAL_STATES, COLORS);
@@ -25,8 +34,10 @@ public class SlimeMold extends Simulation {
 
     @Override
     public void setParameters (Map<String, Double> parameterMap) {
-        // TODO Auto-generated method stub
-
+        myWiggleBias = parameterMap.get(WIGGLE_BIAS);
+        myWiggleAngle = parameterMap.get(WIGGLE_ANGLE);
+        mySniffThreshold = parameterMap.get(SNIFF_THRESHOLD);
+        mySniffAngle = parameterMap.get(SNIFF_ANGLE);
     }
 
 }
